f297d908790ecc8d28ec33d46d13dd1fc0248dad
Harrison Ianatchkov <harrison1993@gmail.com>
2015-02-27 15:22:08 -0500

    Added all methods to be used in the BinaryTree class. Began writing invariants, pre- and post-conditions up to and including step 6 of the assignment

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -1,9 +1,21 @@
+import be.ac.ua.ansymo.adbc.annotations.invariant;
+import be.ac.ua.ansymo.adbc.annotations.requires;
+import be.ac.ua.ansymo.adbc.annotations.ensures;
 
+@invariant ({
+	"this.height() != 0",
+})
 public class BinaryTree {
 	private BinaryTree left;
 	private BinaryTree right;
 	private long id;
 	
+	@requires({
+		"id != null"
+	})
+	@ensures ({
+		"(getLeft() == null && getRight() == null) == true"
+	})
 	public BinaryTree(long id) {
 		this.id = id;
 	}
@@ -11,10 +23,24 @@ public class BinaryTree {
 	public BinaryTree getLeft() {return left;}
 	public BinaryTree getRight() {return right;}
 	
+	@requires ({
+		"iBinTree != null",
+		"this.getLeft() == null"
+	})
+	@ensures ({
+		"getLeft() != null"
+	})
 	public void setLeft(BinaryTree iBinTree) {
 		left = iBinTree;
 	}
 	
+	@requires ({
+		"iBinTree != null",
+		"this.getRight() == null"		
+	})
+	@ensures ({
+		"getRight() != null"
+	})
 	public void setRight(BinaryTree iBinTree) {
 		right = iBinTree;
 	}
@@ -30,15 +56,35 @@ public class BinaryTree {
 	public int sumNodes() {
 		int sum = 1;
 		if (hasLeft() && hasRight())
-			sum += (getLeft().sumNodes() + getRight().sumNodes());
+			return sum + (getLeft().sumNodes() + getRight().sumNodes());
 		else if (hasLeft() && !hasRight())
-			sum += getLeft().sumNodes();
+			return sum + getLeft().sumNodes();
 		else if (!hasLeft() && hasRight())
-			sum += getRight().sumNodes();
+			return sum + getRight().sumNodes();
 		else
 			return sum;
-		
-		return sum;
-			
 	}
+	
+	public int height() {
+		int height = 1;
+		int hL = 0;
+		int hR = 0;
+		
+		if (!hasLeft() && !hasRight())
+			return height;
+		else if (!hasRight())
+			return height + getLeft().height();
+		else if (!hasLeft())
+			return height + getRight().height();
+		else {
+			hL = getLeft().height();
+			hR = getRight().height();
+			if (hR > hL)
+				return height + hR;
+			else 
+				return height + hL;
+		}
+	}
+	
+	
 }
